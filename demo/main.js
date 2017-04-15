121be916bb1c047b61740ee3b6eee94cc5af10e2
Simon <simonxd@gmail.com>
2017-04-15 01:44:28 -0300

    updated syntax

--- demo/main.js
+++
@@ -1,8 +0,0 @@
-define(['comparator'], function (Comparator) {
-
-    var msg = 'Main demo of the comparator module running!';
-
-	console.log(msg);
-
-	alert(msg);
-});
--- test/base.js
+++ test/base.js
@@ -1,132 +1,88 @@
-(function(name, factory) {
+const comparator = require('../');
+const should = require('should');
 
-	var mod = typeof define !== 'function' ?
-		// node
-		'.././src' :
-		// browser
-		'comparator',
-		// dependencies for the test
-		deps = [mod, 'should', 'lodash', 'backbone'];
+describe('comparatorFunc = comparator(props, options)', function () {
 
-	if (typeof define !== 'function') {
-		// node
-		factory.apply(null, deps.map(require));
-	} else {
-		// browser
-		define(deps, factory);
-	}
+	beforeEach(function () {
+		var fruits = this.fruits = [];
 
-})('test', function(comparator, should, _, Backbone) {
-	'use strict';
-
-	describe('comparatorFunc = comparator(props, options)', function () {
-
-		beforeEach(function () {
-			var fruits = this.fruits = [];
-
-			fruits.push({
-				name: 'Orange',
-				price: 3,
-				quantity: 20,
-			});
-
-			fruits.push({
-				name: 'Banana',
-				price: 4,
-				quantity: 25,
-			});
-
-			fruits.push({
-				name: 'Apple',
-				price: 4,
-				quantity: 30,
-			});
+		fruits.push({
+			name: 'Orange',
+			price: 3,
+			quantity: 20,
 		});
 
-		it('Single comparison', function() {
-			var fruits = this.fruits,
+		fruits.push({
+			name: 'Banana',
+			price: 4,
+			quantity: 25,
+		});
+
+		fruits.push({
+			name: 'Apple',
+			price: 4,
+			quantity: 30,
+		});
+	});
+
+	it('Single comparison', function() {
+		var fruits = this.fruits,
 				priceComparator = comparator('price', { price: 1 }),
-				orange = _.findWhere(fruits, { name: 'Orange' }),
-				banana = _.findWhere(fruits, { name: 'Banana' }),
-				apple = _.findWhere(fruits, { name: 'Apple' });
+				orange = fruits[0],
+				banana = fruits[1],
+				apple  = fruits[2];
 
-			priceComparator(orange, banana).should.eql(-1);
-			priceComparator(banana, orange).should.eql(1);
+		priceComparator(orange, banana).should.eql(-1);
+		priceComparator(banana, orange).should.eql(1);
 
-			priceComparator(apple, banana).should.eql(0);
+		priceComparator(apple, banana).should.eql(0);
 
-			priceComparator(apple, orange).should.eql(1);
-		});
+		priceComparator(apple, orange).should.eql(1);
+	});
 
-		it('Multiple comparison, single direction', function () {
+	it('Multiple comparison, single direction', function () {
 
-			var fruits = this.fruits,
+		var fruits = this.fruits,
 				priceComparator = comparator('price'),
 				priceThenNameComparator = comparator(['price','name']),
-				orange = _.findWhere(fruits, { name: 'Orange' }),
-				banana = _.findWhere(fruits, { name: 'Banana' }),
-				apple = _.findWhere(fruits, { name: 'Apple' });
+				orange = fruits[0],
+				banana = fruits[1],
+				apple  = fruits[2];
 
-			priceThenNameComparator(orange, apple).should.eql(-1);
-			priceThenNameComparator(apple, orange).should.eql(1);
+		priceThenNameComparator(orange, apple).should.eql(-1);
+		priceThenNameComparator(apple, orange).should.eql(1);
 
-			priceComparator(apple, banana).should.eql(0);
-			priceThenNameComparator(apple, banana).should.eql(-1);
-			priceThenNameComparator(banana, apple).should.eql(1);
-		});
-
-		it('Multiple comparison using backbone models', function () {
-
-			var fruits = this.fruits,
-				bbFruits = new Backbone.Collection(_.clone(fruits));
-
-			bbFruits.pluck('name').should.eql(_.pluck(fruits, 'name'));
-
-			// set a comparator on bbFruits
-			bbFruits.comparator = comparator(['price','name'], {
-				price: -1,
-				name: -1,
-			}, {
-				root: 'attributes'
-			});
-
-			bbFruits.sort();
-
-			bbFruits.pluck('name').should.eql(['Banana','Apple','Orange']);
-		});
-
-		it('direction simple', function () {
-			var fruits = this.fruits,
-				higherQuantities = comparator('quantity', -1);
-
-			// sort fruits
-			_.pluck(fruits.sort(higherQuantities),'quantity').should.eql([30, 25, 20])
-		})
-
+		priceComparator(apple, banana).should.eql(0);
+		priceThenNameComparator(apple, banana).should.eql(-1);
+		priceThenNameComparator(banana, apple).should.eql(1);
 	});
+
+	// it('Multiple comparison using backbone models', function () {
+
+	// 	var fruits = this.fruits,
+	// 		bbFruits = new Backbone.Collection(_.clone(fruits));
+
+	// 	bbFruits.pluck('name').should.eql(_.pluck(fruits, 'name'));
+
+	// 	// set a comparator on bbFruits
+	// 	bbFruits.comparator = comparator(['price','name'], {
+	// 		price: -1,
+	// 		name: -1,
+	// 	}, {
+	// 		root: 'attributes'
+	// 	});
+
+	// 	bbFruits.sort();
+
+	// 	bbFruits.pluck('name').should.eql(['Banana','Apple','Orange']);
+	// });
+
+	// it('direction simple', function () {
+	// 	var fruits = this.fruits,
+	// 		higherQuantities = comparator('quantity', -1);
+
+	// 	// sort fruits
+	// 	_.pluck(fruits.sort(higherQuantities),'quantity').should.eql([30, 25, 20])
+	// })
+
 });
-
-
-
-/*
-
-define(['underscore.comparator','backbone'], function(undef, Backbone) {
-
-	return function()
-
-		test('Multiple comparison, single direction', function() {
-		});
-
-
-		test('Multiple comparison using backbone models', function() {
-		});
-
-
-
-		test('direction simple setting', function() {
-
-		});
-
-	}
-});
-*/
